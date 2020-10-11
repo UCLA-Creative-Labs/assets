ead7887ad56b4f4536f562cbf22e16dd9fe6cfec
Bryan Pan <bryanpan342@gmail.com>
2020-10-10 18:25:04 -0700

    add react in preparation for gallery mode

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles/App.scss';
+
+function App(): React.Component {
+  return(
+    <div className="wrapper">
+      <h1 className="text">Creative Labs Assets</h1>
+    </div>
+  );
+}
+
+export default App;
